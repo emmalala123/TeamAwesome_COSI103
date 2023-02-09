19b48d6c2b6767327bde3086f68728b0c129fe01
wam99 <wam99@brandeis.edu>
2023-02-09 12:07:37 -0500

    next change

--- hi.py
+++ hi.py
@@ -2,6 +2,6 @@ def hello():
 	print("What's up Will!")
 	print("Nothing much, a bit confused, hbu?")
 	print("I totally get that, but you're doing great!")
-
+	print("Thanks for your help!")
 
 hello()
