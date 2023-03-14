100bf8e79417d54c9188b04b550f24bc11324fee
wam99 <wam99@brandeis.edu>
2023-03-14 16:58:30 -0400

    added my method

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -44,6 +44,21 @@ class GPT():
 
         response = completion.choices[0].text
         return response
+    
+    def willResponse(self,prompt):
+        ''' Generate a GPT response '''
+        prompt = "Generate the python code for the following game:\n" + prompt
+        completion = openai.Completion.create(
+            engine=self.model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        )
+
+        response = completion.choices[0].text
+        return response
 
 if __name__=='__main__':
     '''
