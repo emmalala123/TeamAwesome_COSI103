61be491f9cc39994eef7df1d284889c462129c52
emmalala123 <emmalala@brandeis.edu>
2023-03-14 17:10:35 -0400

    made responseEmma

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -58,6 +58,20 @@ class GPT():
 
         response = completion.choices[0].text
         return response
+    
+    def responseEmma(self,promptEmma):
+        ''' Generate a GPT response '''
+        completion = openai.Completion.create(
+            engine=self.model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        )
+
+        response = completion.choices[0].text
+        return response
 
 if __name__=='__main__':
     '''
