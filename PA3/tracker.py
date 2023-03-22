34c22e7bfab3128ea228c26c3043ddec7ee57585
wam99 <123836439+wam99@users.noreply.github.com>
2023-03-21 20:00:49 -0400

    added show_all and changed db path

--- PA3/tracker.py
+++ PA3/tracker.py
@@ -3,7 +3,7 @@ from transaction import Transaction
 def tracker(): 
 
     db = Transaction()
-    db.add({'item_num': 1, 'amount': 100, 'category': 'food', 'date': '2019-01-01', 'description': 'groceries'})
-    
+    (db.add({'amount': 100, 'category': 'food', 'date': '2019-01-01', 'description': 'groceries'}))
+    print(db.show_all())
 
 tracker()
--- PA3/transaction.py
+++ PA3/transaction.py
@@ -3,23 +3,27 @@ import os
 
 def toDict(t):
     ''' t is a tuple (rowid,title, desc,completed)'''
-    print('t='+str(t))
-    todo = {'rowid':t[0], 'title':t[1], 'desc':t[2], 'completed':t[3]}
+    todo = {'item_num':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
     return todo
 
 class Transaction(): 
 
     def __init__(self): 
         self.runQuery('''CREATE TABLE IF NOT EXISTS transactions 
-        (item_num int, amount int, category text, date text, description text)''',())
+        (amount int, category text, date text, description text)''',())
 
     def add(self,item):
         ''' create a todo item and add it to the todo table '''
-        return self.runQuery("INSERT INTO transactions VALUES(?,?,?,?,?)",(item['item_num'],item['amount'],item['category'],item['date'],item['description']))
+        return self.runQuery("INSERT INTO transactions VALUES(?,?,?,?)",(item['amount'],item['category'],item['date'],item['description']))
+
+    def show_all(self):
+        ''' create a todo item and add it to the todo table '''
+        return self.runQuery("SELECT rowid,* FROM transactions",())
+
 
     def runQuery(self,query,tuple):
         ''' return all of the uncompleted tasks as a list of dicts.'''
-        con= sqlite3.connect(os.getenv('HOME')+'/transaction.db')
+        con= sqlite3.connect('transaction.db')
         cur = con.cursor() 
         cur.execute(query,tuple)
         tuples = cur.fetchall()
